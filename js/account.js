6a4a8e91004a60fc11e383be7f56c877acbe4323
Domestor <Vetal201710@gmail.com>
2025-05-09 15:50:11 +0300

    update js and page

--- js/account.js
+++ js/account.js
@@ -1,13 +1,26 @@
 const userPassword = document.getElementById("password");
 const userConfirmPassword = document.getElementById("confirm-password");
+const userName = document.querySelector('input[name="username"]');
+const userEmail = document.querySelector('input[name="email"]');
 const errorText = document.getElementById("error-text");
 const fillText = document.getElementById("fill-text");
 const createAccountButton = document.getElementById("create-button");
 const toggleButtons = document.querySelectorAll(".lock-button");
-const icon = document.querySelectorAll(".fa-lock");
 
 function validatePassword(event) {
     event.preventDefault();
+
+    if (
+        userName.value.trim() === '' ||
+        userEmail.value.trim() === '' ||
+        userPassword.value.trim() === '' ||
+        userConfirmPassword.value.trim() === ''
+    ) {
+        fillText.classList.remove('hidden');
+        return; 
+    } else {
+        fillText.classList.add('hidden');
+    }
 
     if (userPassword.value !== userConfirmPassword.value) {
         userConfirmPassword.classList.add("border-red-600");
@@ -16,6 +29,16 @@ function validatePassword(event) {
         userConfirmPassword.classList.remove("border-red-600");
         errorText.classList.add("hidden");
     }
+
+    // remove error if input value is correct
+    [userName, userEmail, userPassword, userConfirmPassword].forEach(input => {
+        input.addEventListener('input', () => {
+            input.classList.remove("border-red-600");
+            errorText.classList.add('hidden');
+            fillText.classList.add('hidden');
+        });
+    });
+    
 }
 
 toggleButtons.forEach((button, index) => {

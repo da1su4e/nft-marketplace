b9260ce5dc2a64ec5b5adb38535ca0a10426e3ac
renjii71 <taras.kozak08@gmail.com>
2025-05-11 14:24:00 +0300

    hero section

--- js/script.js
+++ js/script.js
@@ -3,9 +3,17 @@ const mobileMenu = document.getElementById("mobileMenu");
 const exit = document.getElementById("exit");
 
 burgerBtn.addEventListener("click", () => {
-    mobileMenu.classList.remove("left-[-100%]");
-    mobileMenu.classList.add("left-0");
-    exit.classList.remove("hidden");
+    const isOpen = mobileMenu.classList.contains("left-0");
+
+    if (isOpen) {
+        mobileMenu.classList.add("left-[-100%]");
+        mobileMenu.classList.remove("left-0");
+        exit.classList.add("hidden");
+    } else {
+        mobileMenu.classList.remove("left-[-100%]");
+        mobileMenu.classList.add("left-0");
+        exit.classList.remove("hidden");
+    }
 });
 
 exit.addEventListener("click", () => {
